5792c774ff4e9ebc5b9cf9ca44f773c1c537c812
Diego Pagini <diego.pagini@neoris.com>
2023-07-12 15:02:05 +0200

    callback errors

--- src/concepts/02-callbacks.ts
+++ src/concepts/02-callbacks.ts
@@ -4,12 +4,29 @@ import { Hero } from '../interfaces/hero.interface';
 
 export const callbacksComponent = (element: HTMLElement) => {
 	const id = '5d86371f1efebc31def272e2';
-	findHero(id, ({ name }: Hero) => {
-		element.innerHTML = name;
+
+	findHero(id, (error: string, hero: Hero) => {
+		if (error) {
+			element.innerHTML = error;
+			return;
+		}
+
+		element.innerHTML = hero.name;
 	});
 };
 
+/**
+ *
+ * @param {string} id
+ * @param {(error: string | null, hero: Hero) => void} callback
+ */
 const findHero = (id: string, callback: Function) => {
 	const hero = heroes.find((hero) => hero.id === id);
-	callback(hero);
+
+	if (!hero) {
+		callback(`Hero with id ${id} not found.`);
+		return;
+	}
+
+	callback(null, hero);
 };
