10b3a85534e971743a0c73ebcb14ba2368f4732d
Diego Pagini <diego.pagini@neoris.com>
2023-07-13 11:56:08 +0200

    promises

--- src/concepts/03-promises.ts
+++ src/concepts/03-promises.ts
@@ -4,10 +4,33 @@ import { Hero } from '../interfaces/hero.interface';
 
 export const promisesComponent = (element: HTMLElement) => {
 	const id = '5d86371f1efebc31def272e2';
+	const id2 = '5d86371f25a058e5b1c8a65e';
 
-	findHero(id).then((hero) => {
-		element.innerHTML = hero.name;
-	});
+	const renderTwoHeros = (hero1: Hero, hero2: Hero) => {
+		element.innerHTML = `
+    <h3>${hero1.name}</h3>
+    <h3>${hero2.name}</h3>
+  `;
+	};
+
+	// Promise Hell
+	// findHero(id)
+	// 	.then((hero) => {
+	// 		hero1 = hero;
+	// 		findHero(id2)
+	// 			.then((hero2) => {
+	// 				renderTwoHeros(hero1, hero2);
+	// 			})
+	// 			.catch((error: string) => {
+	// 				element.innerHTML = error;
+	// 			});
+	// 	})
+	// 	.catch((error: string) => {
+	// 		element.innerHTML = error;
+	// 	});
+
+	// Solution
+	Promise.all([findHero(id), findHero(id2)]).then(([hero1, hero2]) => renderTwoHeros(hero1, hero2));
 };
 
 const findHero = (id: string): Promise<Hero> => {
